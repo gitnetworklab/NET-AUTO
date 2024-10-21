160d37b688ce454a51f34ea7f9d1b172242a169f
adm-lab-sbodhgir <adm-lab-sbodhgir@email.com>
2024-10-21 23:14:48 +0530

    Updated.

--- connect.py
+++ connect.py
@@ -1,6 +1,6 @@
 from napalm import get_network_driver
 
 driver = get_network_driver('ios')
-device = driver('192.168.1.205', 'robot', 'Automation#987')
+device = driver('192.168.1.205', '*', '*')
 device.open()
 device.close()
